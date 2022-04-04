f2093d4f07a7ef354693ddfc867e4d891e9a6e4a
schm00g <hk8igureu@relay.firefox.com>
2022-04-04 10:03:19 +0100

    calc days old

--- script.js
+++ script.js
@@ -1,2 +1,5 @@
-let d = new Date();
-document.getElementById("date").innerHTML = "<span><b>" + d.toLocaleDateString()  + "</b></span>";
+const writtenDate = new Date(2022, 2, 29);
+const currentDate = new Date();
+const diffTime = Math.abs(currentDate - writtenDate);
+const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+document.getElementById("date").innerHTML = "<span><b>" + diffDays  + " days ago</b></span>";
